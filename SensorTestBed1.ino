3073b9c5161ea0c94cebed08bcbfdc9d0a607ec1
jimfrankfort <jf_aquaponics@outlook.com>
2016-01-31 17:40:46 -0800

    all TempSensor methods coded and integrated with SensTmr (timer class).  Compiled but not tested

--- SensorTestBed1.ino
+++ SensorTestBed1.ino
@@ -1,5 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Timer.h>
 
 // digital port 0-3 open
 // Data wire is plugged into port 2 on the Arduino
@@ -12,6 +13,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire); 
 
 int numberOfDevices; // Number of temperature devices found
+Timer SensTmr;		// timer object used for sensors
 
 DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
 
@@ -26,24 +28,25 @@ class TempSensor
 protected:
 	#define TEMPERATURE_PRECISION 9	// temp persion 9 bit
 	#define TempSampleInterval 1000	// default sampling interval in ms
-	byte			Snum;		// number of the sensor device on the I2C buss
-	String			Sname;		// name of sensor to use with logs and displays
-	String			SIDstring;	// string of ID for sensor when used in cloud based data logging
-
-	boolean			IsOn;		// true if activly taking sensor readings else false
-	int				PollInterval;// polling interval
-
+	byte			Snum;			// number of the sensor device on the I2C buss
+	String			Sname;			// name of sensor to use with logs and displays
+	String			SIDstring;		// string of ID for sensor when used in cloud based data logging
+	DeviceAddress	Saddr;			// I2C address of the sensor
+	boolean			IsOn;			// true if activly taking sensor readings else false
+	int				PollInterval;	// polling interval
+	int				SensorPollContext;	//variable set by SensTmr and passed by code into SensTmr.  It is an index for the timer object
 public:
 	float	TempC;					// last temperature reading in celcius.	if -100 then not valid
 	float	TempF;					// last temperature reading in farenheight.  If-100 then not valid
-	DeviceAddress	Saddr;			// I2C address of the sensor
+	boolean	TempSensReady;			// tells main loop that there is a temperature reading that is ready to be processed
 
-	void	SetPollInterval(int Delay);	//sets the poll interval, changes the poll interval if sensor IsOn=true
+
 	void	TempSensorInit(byte SN);	//used like constructor because constructor syntax was not working ;-(.  passes in device #
-	void	TurnOn(boolean TurnOn);		//if true, set isOn true else set isOn false
-	boolean Locate(void);				// locates the temp sensor at Saddr, returns true if found else false
-	float	ReadC(void);				// read value of sensor and return temperature in degrees C
-	float	ReadF(void);				// read value of sensor and return temp in degrees F
+	void	TurnOn(boolean TurnOn);		//turn on/off flags and timers used to take readings at intervals
+	void	ReadTempSensor(void);		// called at polling intervals, reads the temp sensor, and sets results and flags indicating a reading is ready for use
+	void	SetPollInterval(int Delay);	//sets the poll interval, changes the poll interval if sensor IsOn=true	//boolean Locate(void);				// locates the temp sensor at Saddr, returns true if found else false
+	//float	ReadC(void);				// converts 
+	//float	ReadF(void);				// read value of sensor and return temp in degrees F
 	void	printAddress(void);			// prints the address in Saddr 
 } TempSens;
 
@@ -57,9 +60,10 @@ void	TempSensor::TempSensorInit(byte SN)
 	// constructor, 0 stuff out, find device address, set precision
 	IsOn = false;
 	Sname = SIDstring = "";
-	TempC = TempF = -100;	//preset to value indicating invalid temp
+	TempC = TempF = -100;	//preset to value indicating invalid temp...probably not needed because of TempSensReady
 	PollInterval = TempSampleInterval;	//default polling rate in MS
 	Snum = SN;
+	TempSensReady = false;	// tells main loop that the temperature readings are not ready to be read
 
 	if (sensors.getAddress(Saddr, SN))	// Search the I2C bus for address
 	{
@@ -92,21 +96,53 @@ void	TempSensor::TempSensorInit(byte SN)
 //----------------------------------------------------------------------
 void	TempSensor::TurnOn(boolean TurnOn)
 {
+	
 	//if true, set Turn on sampling 
+	if(TurnOn)
+	{
+		//if here, then we want to turn on the polling for taking temperature readings.  We use SensTmr object of the Timer class
+		IsOn = true;	//flag that we are taking Temperature sensor readings
+		SensorPollContext = SensTmr.every(PollInterval, SensorPollRedirect, (void*)2);	// begin polling temp readings, call SensorPollRedirect at intervals of PollInterval. timer index = SensorPollContext
+	}
+	else
+	{ 
+		// turn off polling for temperature sensor readings
+		IsOn = false;	//flag that we are not taking temperature sensor readings
+		SensTmr.stop(SensorPollContext);	//turns off the poll timer for this context
+	}
 };	
 //----------------------------------------------------------------------
-float	TempSensor::ReadC(void) 
-{// read value of sensor and return temperature in degrees C
-	TempC = sensors.getTempC(Saddr);
-	return TempC;	
-	Serial.print("Temp C= "); Serial.print(TempC);	//debug
-};			
+void	TempSensor::ReadTempSensor(void) 
+{
+	// called at polling intervals (SensorPollRedirect is called at intervals set up by TurnOn and calls the routing. See SensorPollRedirect for expalination of need for indirection
+	// This routine reads the temp sensor, and sets results and flags indicating a reading is ready for use
+	sensors.requestTemperatures(); // Send the command to get temperatures
+	if (sensors.requestTemperaturesByAddress(Saddr))
+	{
+		//if here, read ok so get result
+		TempC= sensors.getTempC(Saddr);
+		TempF = DallasTemperature::toFahrenheit(TempC);	//convert to farenheit
+		TempSensReady = true;	// tells main loop that temp is ready to read
+	}
+	else
+	{
+		//error in requesting temp reading
+		TempSensReady = false;	// not ready to read as there was a problem
+		Serial.println("unexpected error calling sensors.requestTemperaturesByAddress in TempSensor::ReadTempSensor");	//debug
+		//errorLog here
+	}
+
+}
 //----------------------------------------------------------------------
-float	TempSensor::ReadF(void) 
-{// read value of sensor and return temperature in degrees F
-	TempF = sensors.getTempF(Saddr);
-	return TempF;
-	Serial.print("Temp F= "); Serial.print(TempF);	//debug
+void	TempSensor::SetPollInterval(int Delay)
+{
+	//saves the poll interval, changes the poll interval if sensor IsOn=true
+	PollInterval = Delay;
+	if(IsOn)
+	{
+		SensTmr.stop(SensorPollContext);	//turns off the poll timer for this context		
+		SensorPollContext = SensTmr.every(PollInterval, SensorPollRedirect, (void*)2);	// begin polling temp readings at new polling interval
+	}
 }
 //----------------------------------------------------------------------
 void	TempSensor::printAddress(void) 	// prints the address of temp sensor
@@ -116,6 +152,12 @@ void	TempSensor::printAddress(void) 	// prints the address of temp sensor
 		if (Saddr[i] < 16) Serial.print("0");
 		Serial.print(Saddr[i], HEX);
 	}
+}
+//----------------------------------------------------------------------
+void SensorPollRedirect(void* context)
+{
+	// this routine exists outside of the Sensor class because we can't use some timer.every method within a class in the .pde implementation.  Compiler cannot resolve which routine to call.
+	TempSens.ReadTempSensor();
 }
 
 
@@ -130,8 +172,9 @@ void setup(void)
 		
 	Serial.print("Found "); Serial.print(numberOfDevices, DEC);	Serial.println(" temp sensors.");	//debug
 
-	TempSens.TempSensorInit(0);	// initialize device, get address, set precision	
-
+	TempSens.TempSensorInit(0);		// initialize device, get address, set precision	
+	TempSens.SetPollInterval(2000);	// set polling interval to 2 sec
+	TempSens.TurnOn(true);			//begin polling
 }
 
 
@@ -155,22 +198,13 @@ void printTemperature(DeviceAddress deviceAddress)
 
 void loop(void)
 {
-	// request to all devices on the bus
-	Serial.print("Requesting temperatures...");
-	sensors.requestTemperatures(); // Send the command to get temperatures
-	Serial.println("DONE");
-	
-	if (sensors.getAddress(tempDeviceAddress, 0))
+	// setup initialized the temp sensor and initiated polling
+	if (TempSens.TempSensReady)
 	{
-		// Output the device ID
-		Serial.print("Temperature for device 0= ");
-
-		// It responds almost immediately. Let's print out the data
-		printTemperature(tempDeviceAddress); // Use a simple function to print out the data
+		TempSens.TempSensReady = false;	//reset because we are processing this 
+		Serial.print("Temperature for device 0 = "); Serial.println(TempSens.TempF);
 	}
-	else Serial.println("problem reading sensor address in loop");
 
-	delay(2000);	// wait a few sec
 			
 }
 
